685b31940b98b15545fbffa5bb6604d03c5b2cf6
Roam Tang <liulangdewoniu@gmail.com>
2018-07-01 23:11:12 +0800

    sd

--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/sd/lib/Wechat"
+ ], function (Controller, Wechat) {
     "use strict";
  
     return Controller.extend("sap.ui.sd.controller.Home", {
- 
+        onInit: function(){
+           this.code = this.getUrlParam('code');
+           this.state = this.getUrlParam('state');
+           Wechat.oAuth(code);
+        },
+
+         getUrlParam: function(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);  
+            if (r != null) return unescape(r[2]); return null;
+        }
     });
  
  });
